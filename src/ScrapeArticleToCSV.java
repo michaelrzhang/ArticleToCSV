d01ee487b3d551e0c6a6c1eeb423eaaea4c3f583
Michael Zhang <michaelzhang333@gmail.com>
2014-12-26 13:41:36 -0800

    Added while true loop to read multiple lines of input

--- src/ScrapeArticleToCSV.java
+++ src/ScrapeArticleToCSV.java
@@ -13,25 +13,50 @@ public class ScrapeArticleToCSV{
 		//Take a HTTP url as input
 		Scanner inp = new Scanner(System.in);
 		String outputFile = "sentences.csv";
-		System.out.print("Enter a url for a news article: ");
-		String link_target = inp.next();
-		
-		List<String> text = Fetcher.pullAndExtract(link_target); 
+		String link_target = "";
+		List<String> text; 
 		InputStream modelInput = null;
-		SentenceModel model;
-		try{
-			//Creates a model for sentence detector
-			modelInput = new FileInputStream("en-sent.bin");
-			model = new SentenceModel(modelInput);
-			SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+		SentenceModel model = null;
+		SentenceDetectorME sentenceDetector = null;
+		List<String[]> sentences = new ArrayList<String[]>();
 
-			//Stores article into a List of string arrays, with each array a sentence
-			List<String[]> sentences = new ArrayList<String[]>();
-			for (int i=0;i<text.size();i++){
-				sentences.add(sentenceDetector.sentDetect(text.get(i)));
+		while (true){
+			System.out.print("Enter an url for a news article. Enter . to stop: ");
+			link_target = inp.next();
+			if (link_target.equals("."))
+				break;
+			try{
+				//Creates a model for sentence detector
+				if (modelInput == null){
+					modelInput = new FileInputStream("en-sent.bin");
+					model = new SentenceModel(modelInput);
+					sentenceDetector = new SentenceDetectorME(model);
+					System.out.println("initilized modelInput");
+				}
+
+				//Stores article into a List of string arrays, with each array a sentence
+				text = Fetcher.pullAndExtract(link_target);
+				for (int i=0;i<text.size();i++){
+					sentences.add(sentenceDetector.sentDetect(text.get(i)));
+				}
+				sentences.add(sentenceDetector.sentDetect("Next Article.")); // Seperates each article in output file
 			}
-
-			//Write sentences into CSV format
+			catch (IOException e) {
+			 	e.printStackTrace();
+			}
+			finally {
+			  	if (modelInput != null) {
+					try {
+				  		modelInput.close();
+					}
+				catch (IOException e) {
+					}
+			  	}
+			}
+		}
+		
+		//Write sentences into CSV format
+		try{
 			CsvWriter csvOutput = new CsvWriter(new FileWriter(outputFile, true), ',');
 			for (int i=0; i < sentences.size(); i++){
 				for(int j=0; j < sentences.get(i).length; j++){
@@ -41,19 +66,11 @@ public class ScrapeArticleToCSV{
 				}
 			}
 			csvOutput.close();
+			System.out.println(". detected. Stopping program");
 		}
 		catch (IOException e) {
-		 	e.printStackTrace();
-		}
-		finally {
-		  	if (modelInput != null) {
-				try {
-			  		modelInput.close();
-				}
-			catch (IOException e) {
-				}
-		  	}
-		}
+			 	e.printStackTrace();
+			}
 		
 	}
 
